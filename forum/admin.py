10bff8e65adf638da19d44c03fdcec04ee7f5522
jamcoy <james@coynemail.net>
2016-11-25 19:04:19 +0000

    Rename Vote class to PollVote

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Subject, Thread, Post
+from .models import Subject, Thread, Post, Poll, PollSubject, PollVote
 
 admin.site.register(Subject)
 admin.site.register(Thread)
 admin.site.register(Post)
+admin.site.register(Poll)
+admin.site.register(PollSubject)
+admin.site.register(PollVote)
--- forum/models.py
+++ forum/models.py
@@ -42,7 +42,7 @@ class PollSubject(models.Model):
         return self.name
 
 
-class Vote(models.Model):
+class PollVote(models.Model):
     poll = models.ForeignKey(Poll, related_name="votes")
     subject = models.ForeignKey(PollSubject, related_name="votes")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='votes')

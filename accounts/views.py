131f0f75eca5f49c72ccb7a4c920eb96d32cb2a8
jamcoy <james@coynemail.net>
2016-11-25 09:11:22 +0000

    Basic views for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def register(request):
-    return render(request, 'index.html')
+    return render(request, 'www/index.html')
 
 
 def login(request):
-    return render(request, 'index.html')
+    return render(request, 'www/index.html')
 
 
 def logout(request):
-    return render(request, 'index.html')
+    return render(request, 'www/index.html')
 
 
 def profile(request):
-    return render(request, 'index.html')
+    return render(request, 'www/index.html')

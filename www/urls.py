ccb728c05ce13ff99d187d6d4d00173e854411f9
jamcoy <james@coynemail.net>
2016-11-25 17:01:18 +0000

    Add footer and social media buttons

--- www/urls.py
+++ www/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^contact/$', views.contact),
+    url(r'^about/$', views.about),
+    url(r'^terms/$', views.terms),
 ]
--- www/views.py
+++ www/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'www/index.html')
+
+
+def about(request):
+    return render(request, 'www/about.html')
+
+
+def contact(request):
+    return render(request, 'www/contact.html')
+
+
+def terms(request):
+    return render(request, 'www/terms.html')

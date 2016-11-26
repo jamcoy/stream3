a22e1e8c1f48a2a7d1a0a5ee5c343ad39c791ca0
jamcoy <james@coynemail.net>
2016-11-26 10:05:26 +0000

    Add stripe related dispatchers

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
+    # user accounts
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+
+    # stripe
+    url(r'^cancel_subscription/$', views.cancel_subscription, name='cancel_subscription'),
+    url(r'^subscriptions_webhook/$', views.subscriptions_webhook, name='subscriptions_webhook'),
+
 ]

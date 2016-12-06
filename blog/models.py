fe087a030a0e4c7a045578821802bb2a35456249
jamcoy <james@coynemail.net>
2016-12-06 12:17:50 +0000

    Add tinyMCE to blog form

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     # user in the 'auth_user' table.
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=200)
-    content = HTMLField()
+    content = HTMLField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
     views = models.IntegerField(default=0)  # Record how often a post is seen

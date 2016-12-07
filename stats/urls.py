3ed405299845aa0499b65691f2e184272bd9c98e
jamcoy <james@coynemail.net>
2016-12-07 15:59:14 +0000

    Wire-up the buttons on the home page

--- stats/urls.py
+++ stats/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views  # can probably shorten to import views
 
 urlpatterns = [
-    url(r'^$', views.economy_stats),
+    url(r'^$', views.economy_stats, name='stats'),
     url(r'^select_model/$', views.select_model, name="select_model"),
     url(r'^select_year/$', views.select_year, name="select_year"),
     url(r'^select_sub_details/$', views.select_sub_details, name="select_sub_details"),

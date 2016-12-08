18a6bcce589332ad091941467cd80629508557eb
jamcoy <james@coynemail.net>
2016-12-08 16:59:41 +0000

    Move tinyMCE init to its own file to avoid duplication

---
+++ forum/static/js/tinyMCE_init.js
@@ -0,0 +1,8 @@
+tinyMCE.init({
+    mode: "textareas",
+    theme: "modern",
+    plugins: "paste emoticons image",
+    image_dimensions: false,
+    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist \
+              numlist outdent indent | emoticons | image"
+});

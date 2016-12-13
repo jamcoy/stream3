4f8cf9ffb0bb3f5c3da9173c192a83c9b28158da
jamcoy <james@coynemail.net>
2016-12-13 13:20:20 +0000

    Config wsgi so gunicorn uses the production settings

--- stream_3/wsgi.py
+++ stream_3/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for we_are_social project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
+import settings.production
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stream_3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
 application = get_wsgi_application()
